a6bb01459652ebbcbb64b93c66e0041d2dced63d
Chris Hile <chile@student.fullsail.edu>
2022-08-26 11:56:06 -0400

    corrected

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const validate = require("../validation/validation"
+)
 
 router.get("/",(req, res) => {
     res.render('index', {
@@ -28,9 +30,25 @@ router.get("/projects",(req, res) => {
 router.get("/registration",(req, res) => {
     res.render('registration', {
         pagename: "Registration",
+        
     });
+   
 });
 
+router.post("/registration", (req, res) => {
+    const errors = validate(req);
+    if(Object.keys(errors).length === 0){
+        errors.message = "Registration Successful"
+    } else {
+        errors.message = 'Registration failed'
+    }
 
+    console.log(req.body)
+    res.render('registration', {
+        pagename: "Registration",
+        errors: errors,
+        input: input,
+    });
+});
 
 module.exports = router;
